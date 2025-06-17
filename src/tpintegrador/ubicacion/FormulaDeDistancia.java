5d9d56bb045c42fa2a1c8a5e662f5ee9c116dabd
Facundo Mosquera <facundomosquerabus@gmail.com>
2025-06-17 20:59:44 -0300

    Ubicacion

---
+++ src/tpintegrador/ubicacion/FormulaDeDistancia.java
@@ -0,0 +1,6 @@
+package tpintegrador.ubicacion;
+
+public interface FormulaDeDistancia {
+
+	public double distanciaEntre(Ubicacion u1, Ubicacion u2);
+}
---
+++ src/tpintegrador/ubicacion/Ubicacion.java
@@ -0,0 +1,11 @@
+package tpintegrador.ubicacion;
+
+public interface Ubicacion {
+
+	    public double getLatitud();
+		
+		public double getLongitud();
+		
+		public double distanciaA(Ubicacion u);		
+
+}
