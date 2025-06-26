18d30a8f5714b0578167b488b4339504ae9d0f62
Ian <Ian@DESKTOP-CQP7KFJ>
2025-06-26 00:15:12 -0300

    Se agrego test Organizacion

--- test/tpintegrador/organizacion/OrganizacionTest.java
+++ test/tpintegrador/organizacion/OrganizacionTest.java
@@ -1,19 +1,76 @@
 package tpintegrador.organizacion;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*; 
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.*;
+
+import tpintegrador.Evento;
+import tpintegrador.muestra.*;
+import tpintegrador.zonadecobertura.*;
+import tpintegrador.ubicacion.*;
+
 
 class OrganizacionTest {
 
+	private Organizacion org;
+	private FuncionalidadExterna funcionalidadNueva;
+	private FuncionalidadExterna funcionalidadVerificada;
+	private ZonaDeCobertura zona;
+	private Muestra muestra;
+	
 	@BeforeEach
-	void setUp() throws Exception {
+	public void setUp() {
+		org = new Organizacion();
+		funcionalidadNueva = mock(FuncionalidadExterna.class);
+		funcionalidadVerificada = mock(FuncionalidadExterna.class);
+		zona = mock(ZonaDeCobertura.class);
+		muestra = mock(Muestra.class);
+		
+		org.setFuncionalidadNuevaMuestra(funcionalidadNueva);
+		org.setFuncionalidadVerificacion(funcionalidadVerificada);
 	}
-
+	
+	@Test 
+	public void updateConNuevaMuestraEjecutaFuncionalidadNueva() {
+		org.update(zona, Evento.NUEVA_MUESTRA, muestra);
+		
+		verify(funcionalidadNueva, times(1)).nuevoEvento(org, zona, muestra);
+		verify(funcionalidadVerificada, never()).nuevoEvento(any(), any(), any());
+	}
+	
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+	public void updateConVerificacionEjecutaFuncionalidadVerificacion() {
+        org.update(zona, Evento.VERIFICACION, muestra);
 
+        verify(funcionalidadVerificada, times(1)).nuevoEvento(org, zona, muestra);
+        verify(funcionalidadNueva, never()).nuevoEvento(any(), any(), any());
+    }
+
+	
+	@Test 
+	public void puedeActualizarFuncionalidadesDinanimicas() {
+		FuncionalidadExterna nuevaFuncion = mock(FuncionalidadExterna.class);
+		org.setFuncionalidadNuevaMuestra(nuevaFuncion);
+		
+		org.update(zona, Evento.NUEVA_MUESTRA, muestra);
+		
+		verify(nuevaFuncion, times(1)).nuevoEvento(org, zona, muestra);
+		verify(funcionalidadNueva, never()).nuevoEvento(any(), any(), any());
+	}
+	
+	@Test
+	public void getters() {
+		Ubicacion ubicacion = mock(Ubicacion.class);
+		org.setUbicacion(ubicacion);
+		org.tipoDeOrganizacion(TipoDeOrganizacion.SALUD);
+		org.cantTrabajadores(50);
+		
+		assertEquals(org.getUbicacion(), ubicacion);
+		assertEquals(org.getTipoOrg(), TipoDeOrganizacion.SALUD);
+		assertEquals(org.getCantTrabajadores(), 50);
+	}
+	
 }
